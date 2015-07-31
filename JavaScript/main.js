f16c0d415a26b93e197ec468088d1736fd83193d
Alexandru Botez <botez14alexandru@gmail.com>
2015-07-31 23:30:05 +0300

    implementing funtionality for portolio filter

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,19 @@
 $(function () {
 
+
+
+    function fade( group ) {
+        $('.portofolio_pictures li')
+            .fadeOut()
+            .promise()
+            .done( function() {
+                console.log( group );
+                $('.portofolio_pictures ' + group).fadeIn();
+            });
+    }
+
+    $('input[name$="group1"]').click(function () {
+        fade( $(this).val() );
+    });
+
 });
