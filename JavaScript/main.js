905a33251a45fd5ea3673d3eb7f9383c5df9d784
Alexandru Botez <botez14alexandru@gmail.com>
2015-08-03 10:35:34 +0300

    320 px text align for section deleted form validation that didn't work

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,15 @@
 $(function () {
 
 
-
-    function fade( group ) {
-        $('.portofolio_pictures li')
-            .fadeOut()
-            .promise()
-            .done( function() {
-                $('.portofolio_pictures ' + group).fadeIn();
-            });
+    function fade(group) {
+        $('.portofolio_pictures li').fadeOut().promise().done(function () {
+            $('.portofolio_pictures ' + group).fadeIn();
+        });
     }
 
     $('input[name$="group1"]').click(function () {
-        fade( $(this).val() );
+        fade($(this).val());
     });
+
 
 });
